c30bf3c4e1b3237363c32cc44d210f26e2695d0e
MapleBurn <miki.holan@outlook.cz>
2025-02-14 07:41:12 +0100

    pleb

--- gitlukoraptor/.godot/mono/temp/obj/Debug/GitLukoraptor.AssemblyInfo.cs
+++ gitlukoraptor/.godot/mono/temp/obj/Debug/GitLukoraptor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GitLukoraptor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f4c2a1d7452da79fa34cb512eaf5b36a1840fe9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70e88c720b563fe08f795471f5eca346882c449f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GitLukoraptor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GitLukoraptor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
