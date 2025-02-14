70e88c720b563fe08f795471f5eca346882c449f
MapleBurn <miki.holan@outlook.cz>
2025-02-14 07:13:44 +0100

    swapping scenes

--- gitlukoraptor/.godot/mono/temp/obj/Debug/GitLukoraptor.AssemblyInfo.cs
+++ gitlukoraptor/.godot/mono/temp/obj/Debug/GitLukoraptor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GitLukoraptor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19ba47f8ee77d815d204461d6bb1dc8ddd5a3a37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f4c2a1d7452da79fa34cb512eaf5b36a1840fe9")]
 [assembly: System.Reflection.AssemblyProductAttribute("GitLukoraptor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GitLukoraptor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- gitlukoraptor/Tutorial.cs
+++ gitlukoraptor/Tutorial.cs
@@ -3,8 +3,9 @@ using System;
 
 public partial class Tutorial : Node2D
 {
-	[Export]
-	private PackedScene timer {  get; set; } 
+	//[Export]
+	//private PackedScene timer {  get; set; } 
+	private int time = 0;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -15,5 +16,14 @@ public partial class Tutorial : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+    private void OnTimerTimeout()
+	{
+		time++;
+		if (time > 2)
+		{
+			GetTree().ChangeSceneToFile("res://world_1.tscn");
+		}
 	}
 }
