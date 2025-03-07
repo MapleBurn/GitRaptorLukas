c16626d64045c89fb397dd416656d20ee37cf379
MapleBurn <miki.holan@outlook.cz>
2025-03-07 21:21:33 +0100

    made plebs spawn

--- gitlukoraptor/scripts/Pleb.cs
+++ gitlukoraptor/scripts/Pleb.cs
@@ -8,12 +8,17 @@ public partial class Pleb : CharacterBody2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{ 
-
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		
+	}
 
+	private void MoveTime_Tick()
+	{
+		Position += new Vector2(rdm.Next(-20, 20), rdm.Next(-20, 20));
 	}
 }
