35f0812c10e03bd6e0c8e3b148c81d93256c056e
MapleBurn <miki.holan@outlook.cz>
2025-04-09 19:38:09 +0200

    bush

--- gitlukoraptor/scripts/BushSpawner.cs
+++ gitlukoraptor/scripts/BushSpawner.cs
@@ -1,10 +1,11 @@
 using Godot;
 using System;
+using GitLukoraptor.scripts;
+using Environment = Godot.Environment;
 
 public partial class BushSpawner : Node2D
 {
 	[Export] public TileMapLayer GroundTiles;
-	//[Export] public TileMapLayer NatureTiles;
 	private PackedScene _bush;
 	private Random rnd = new Random();
 	
@@ -15,16 +16,48 @@ public partial class BushSpawner : Node2D
 		foreach (Vector2I cellCoords in GroundTiles.GetUsedCells())
 		{
 			Vector2I cellData = GroundTiles.GetCellAtlasCoords(cellCoords);
-			if (cellData != null && cellData == new Vector2I(2, 0))
+			if (cellData == new Vector2I(2, 0))
 			{
-				if (rnd.Next(0, 100) <= 10)
+				if (rnd.Next(0, 100) <= 10 && IsSurroundingField(cellCoords))
 				{
-					//NatureTiles.SetCell(cellCoords, 0, new Vector2I(0, 0));
-					Bush bush = (Bush)_bush.Instantiate();
-					AddChild(bush);
-					bush.Position = (cellCoords * 16) + new  Vector2I(8, 8);
+					if (Nature.AllowBushSpawn(cellCoords))
+					{
+						Bush bush = (Bush)_bush.Instantiate();
+						AddChild(bush);
+						bush.Position = (cellCoords * 16) + new Vector2I(8, 8);
+						Nature.SpawnBush(cellCoords);
+					}
 				}
 			}
 		}
+	}
+
+	private bool IsSurroundingField(Vector2I coords)
+	{
+		var around = new Vector2I[] { new Vector2I(-1,-1), new Vector2I(0,-1),new Vector2I(1,-1),new Vector2I(-1,0),new Vector2I(1,0),new Vector2I(-1,1),new Vector2I(0,1),new Vector2I(1,1) };
+		foreach (Vector2I coord in around)
+		{
+			if (GetTileType(coords + coord) != "field")
+				return false;
+		}
+		return true;
+	}
+	
+	private string GetTileType(Vector2I coords)
+	{
+		Vector2I cellData = GroundTiles.GetCellAtlasCoords(coords);
+		if (cellData == new Vector2I(0, 0))
+			return "shallow";
+		if (cellData == new Vector2I(1, 0))
+			return "water";
+		if (cellData == new Vector2I(2, 0))
+			return "field";
+		if (cellData == new Vector2I(3, 0))
+			return "forest";
+		if (cellData == new Vector2I(4, 0))
+			return "sand";
+		if (cellData == new Vector2I(5, 0))
+			return "mountain";
+		return "error";
 	}
 }
---
+++ gitlukoraptor/scripts/Nature.cs
@@ -0,0 +1,77 @@
+﻿using Godot;
+using System;
+
+namespace GitLukoraptor.scripts;
+
+public class Nature
+{
+    private static int[][] _bushHeatMap;
+    private static int[][] _spawnedBush;
+
+    private const int BUSH_PRINT_SIZE = 10;
+    
+    static Nature()
+    {
+        _bushHeatMap = new int[512][];
+        for (int i = 0; i < 512; i++)
+        {
+            _bushHeatMap[i] = new int[512];
+        }
+
+        _spawnedBush = CreateBushPrint(BUSH_PRINT_SIZE);
+    }
+
+    public static bool AllowBushSpawn(Vector2I position)
+    {
+        return _bushHeatMap[position.Y][position.X] < 5;
+    }
+
+    public static void SpawnBush(Vector2I position)
+    {
+        for (int i = 0; i < _spawnedBush.Length; i++)
+        {
+            for (int j = 0; j < _spawnedBush.Length; j++)
+            {
+                _bushHeatMap[position.Y + j - BUSH_PRINT_SIZE/2][position.X + i - BUSH_PRINT_SIZE/2] += _spawnedBush[i][j];
+                if (_bushHeatMap[position.Y + j - BUSH_PRINT_SIZE / 2][position.X + i - BUSH_PRINT_SIZE / 2] > 100)
+                    _bushHeatMap[position.Y + j - BUSH_PRINT_SIZE / 2][position.X + i - BUSH_PRINT_SIZE / 2] = 100;
+            }
+        }
+
+        
+        
+    }
+    
+    private static int[][] CreateBushPrint(int size)
+    {
+        int[][] array = new int[size][];
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = new int[size];
+        }
+
+        double centerX = (double)(size - 1) / 2.0;
+        double centerY = (double)(size - 1) / 2.0;
+        double maxRadius = (double)size / 2.0;
+
+        const int centerValue = 100;
+        const int edgeValue = 5;
+        const double valueRange = centerValue - edgeValue;
+        
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                double distance = Math.Sqrt(Math.Pow(i - centerY, 2) + Math.Pow(j - centerX, 2));
+                double normalizedDistance = (maxRadius > 0) ? Math.Min(1.0, distance / maxRadius) : 0;
+                double calculatedValue = centerValue - (valueRange * normalizedDistance);
+                array[i][j] = Math.Max(edgeValue, Math.Min(centerValue, (int)Math.Round(calculatedValue)));
+            }
+        }
+        if (size > 0)
+        {
+             array[size / 2][size / 2] = centerValue;
+        }
+        return array;
+    }
+}
